4e88c953bd8e52a5f345409310ca995608dd6617
Koleneko <vkoleneko@gmail.com>
2021-12-24 18:43:24 +0300

    Initial commit

---
+++ src/review/review.model.ts
@@ -0,0 +1,23 @@
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+  @prop()
+  _id: string;
+
+  @prop()
+  name: string;
+
+  @prop()
+  title: string;
+
+  @prop()
+  description: string;
+
+  @prop()
+  rating: number;
+
+  @prop()
+  createdAt: Date;
+}
---
+++ src/review/review.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { ReviewController } from './review.controller';
+
+@Module({
+  controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModule,
+        schemaOptions: {
+          collection: 'Review',
+        },
+      },
+    ]),
+  ],
+})
+export class ReviewModule {}
